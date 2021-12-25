8531852f9b3715061796ca2121007c7e72cd27b8
glimm <glimm@LAPTOP-R7LUE2JN>
2021-12-25 16:44:49 +0100

    Add project files.

---
+++ Labb-TDD-Bowling/Game.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.Serialization.Formatters;
+using System.Timers;
+
+namespace Labb_TDD_Bowling
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[22];
+        private int _currentRoll;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int roll = 0;
+
+            for (int round = 0; round < 10; round++)
+            {
+                if (IsStrike(roll))
+                {
+                    score += ExtraPointStrike(roll);
+                    roll++;
+                }
+                else if (IsSpare(roll))
+                {
+                    score += 10 + _rolls[roll + 2];
+                    roll += 2;
+                }
+                else
+                {
+                    score += ExtraPointsSpare(roll);
+                    roll += 2;
+                }
+
+            }
+
+            return score;
+        }
+
+        private int ExtraPointsSpare(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1];
+        }
+
+        private int ExtraPointStrike(int roll)
+        {
+            return 10 + _rolls[roll + 1] + _rolls[roll + 2];
+        }
+
+        private bool IsSpare(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1] == 10;
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return _rolls[roll] == 10;
+        }
+
+    }
+}
---
+++ Labb-TDD-Bowling_ALT/Game.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_TDD_Bowling_ALT
+{
+    internal class Game
+    {
+        private int totalScore;
+
+        public int[,] frames = new int[11, 2];
+
+        private int roundCount;
+        private int rollCount = 1;
+        private int strikeScore;
+        private int extraScoreRolls;
+        private int count;
+        private bool spare;
+
+        public int[] rolls = new int[22];
+        private int currentRoll;
+
+        public void Roll(int v)
+        {
+            frames[roundCount, rollCount] = v;
+
+            if (extraScoreRolls != 0)
+            {
+                strikeScore += v;
+                extraScoreRolls--;
+                count++;
+
+                if (count != 0) // gör metod IsSpare och IsStrike
+                {
+                    if (count == 2) //Stike scoreing
+                    {
+                        if (frames[roundCount, 1] + frames[roundCount, 0] != 10)
+                            frames[roundCount - 1, 1] += strikeScore;
+                        else
+                            frames[roundCount - 2, 1] += strikeScore;
+                        totalScore += strikeScore;
+                        strikeScore = 0;
+                        count = 0;
+
+                    }
+                    else if (spare)
+                    {
+                        frames[roundCount - 1, 1] += strikeScore;
+                        totalScore += strikeScore;
+                        spare = false;
+                    }
+                }
+            }
+
+            if (rollCount == 0 || v == 10)
+            {
+
+                roundCount++;
+
+                if (v == 10 && roundCount < 9) // Strike
+                {
+
+                    extraScoreRolls = 2;
+                }
+                else if (v + frames[roundCount - 1, 1] == 10 && roundCount < 9) //Spare
+                {
+                    extraScoreRolls = 1;
+                    spare = true;
+                }
+
+                rollCount = 1;
+
+            }
+            else
+                rollCount--;
+
+            totalScore += v;
+        }
+
+        public int Score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
